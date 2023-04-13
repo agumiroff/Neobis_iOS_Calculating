be1993b82f9ab0b125135f8dda938a642d509143
Askhat Gumirov <agumirovz@gmail.com>
2023-04-14 04:39:49 +0600

    Initial Commit

---
+++ Neobis_iOS_Calculating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Calculating
+//
+//  Created by G G on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
